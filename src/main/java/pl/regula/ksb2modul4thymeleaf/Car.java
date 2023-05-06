2613da5fa7bb1225cbbdbb39924fed08dcbf79f9
monikaRegula <monikaregula97@gmail.com>
2023-05-06 18:57:22 +0200

    Iteration added

--- src/main/java/pl/regula/ksb2modul4thymeleaf/Car.java
+++ src/main/java/pl/regula/ksb2modul4thymeleaf/Car.java
@@ -1,2 +1,31 @@
-package pl.regula.ksb2modul4thymeleaf;public class Car {
+package pl.regula.ksb2modul4thymeleaf;
+
+public class Car {
+
+    private String model;
+    private String mark;
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    public Car(String model, String mark) {
+        this.model = model;
+        this.mark = mark;
+    }
+
+    public Car() {
+    }
 }
--- src/main/java/pl/regula/ksb2modul4thymeleaf/CarController.java
+++ src/main/java/pl/regula/ksb2modul4thymeleaf/CarController.java
@@ -4,14 +4,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
-public class HelloController {
+import java.util.Arrays;
+import java.util.List;
 
-    @GetMapping("/elo")
-    public String get(Model model){
+@Controller
+public class CarController {
+
+    @GetMapping("/car")
+    public String get(Model model) {
         Car car = new Car("BMW", "i8");
+        Car car1 = new Car("Fiat", "Uno");
+        Car car2 = new Car("Opel", "Astra");
+        Car car3 = new Car("Seat", "Ibiza");
+        List<Car> cars = Arrays.asList(car, car2, car3, car1);
+
+        model.addAttribute("cars",cars);
         model.addAttribute("car", car);
         model.addAttribute("name", "Monia");
-        return "elo";
+
+        return "car";
     }
 }
